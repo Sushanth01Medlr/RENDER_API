50dbc27f77a18fcabddff7626eadafeb9ff4add6
Sushanth01Medlr <sushanth@medlr.in>
2025-05-03 11:37:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ class PriceResponse(BaseModel):
 from litestar import get, Litestar
 
 @get("/")  # Handles GET (and HEAD by virtue of GET)
+@head("/")
 async def health_check() -> dict[str, str]:
     return {"status": "alive"}
 
